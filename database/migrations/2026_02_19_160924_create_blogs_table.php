8939776263f28e064e6e364832d0688577e038c8
dev.sagartmg <dev.sagartmg@gmail.com>
2026-04-11 21:50:38 +0545

    feat: ad meta infos in blogs

--- database/migrations/2026_02_19_160924_create_blogs_table.php
+++ database/migrations/2026_02_19_160924_create_blogs_table.php
@@ -12,22 +12,26 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('blogs', function (Blueprint $table) {
-            $table->id();   
-            $table->string('category'); 
-            $table->string('title'); 
-            $table->string('slug')->unique(); 
-            $table->text('content'); 
+            $table->id();
+            $table->string('category');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('content');
             $table->text('readingTime');
             $table->foreignId('user_id')
-                ->nullable()             
-                ->constrained()          
+                ->nullable()
+                ->constrained()
                 ->nullOnDelete();
-            $table->string('image_url')->nullable(); 
-            $table->boolean('is_published')->default(false); 
-            $table->unsignedBigInteger('initial_view_count')->default(0); 
-            $table->unsignedBigInteger('view_count')->default(0); 
-            $table->boolean('is_featured')->default(false); 
-            $table->timestamps(); 
+            $table->string('image_url')->nullable();
+            $table->boolean('is_published')->default(false);
+            $table->unsignedBigInteger('initial_view_count')->default(0);
+            $table->unsignedBigInteger('view_count')->default(0);
+            $table->boolean('is_featured')->default(false);
+            $table->string('meta_title')->nullable();
+            $table->text('meta_description')->nullable();
+            $table->string('meta_keywords')->nullable();
+            $table->string('meta_image')->nullable();
+            $table->timestamps();
         });
     }
 
